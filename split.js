ebbae95b914356e44ca53e1ab4a698585c7125fe
vmik7 <vmik7@yandex.ru>
2021-06-14 13:26:45 +0300

    Realized split with 1-char divider

--- split.js
+++ split.js
@@ -1,5 +1,22 @@
 function split(str, div) {
-    return [];
+    let result = [];
+    if (div.length === 1) {
+        let cur = '';
+        for (let char of str) {
+            if (char === div) {
+                result.push(cur);
+                cur = '';
+            }
+            else {
+                cur += char;
+            }
+        }
+        result.push(cur);
+    }
+    else {
+        
+    }
+    return result;
 }
 
 module.exports.split = split;

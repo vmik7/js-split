70a320b8aeafd5d707d5e7aebd13c07f7d7387ef
vmik7 <vmik7@yandex.ru>
2021-06-14 15:24:43 +0300

    realized multi-character divider

--- split.js
+++ split.js
@@ -1,21 +1,52 @@
+const { resourceLimits } = require("worker_threads");
+
 function split(str, div) {
-    let result = [];
-    if (div.length === 1) {
-        let cur = '';
+    
+    //  extreme cases
+    if (div === undefined) {
+        return [str]; 
+    }
+    if (div === '') {
+        let result = [];
         for (let char of str) {
-            if (char === div) {
-                result.push(cur);
-                cur = '';
-            }
-            else {
-                cur += char;
-            }
+            result.push(char);
         }
-        result.push(cur);
+        return result;
     }
-    else {
-        
+
+    // resulting array
+    let result = [];
+
+    // new string with a div-prefix
+    let s = div + str;
+
+    // calc prefix-function array
+    let p = [0];
+    for (let i = 1; i < s.length; i++) {
+        let j = p[i - 1];
+        while (j >= 0) {
+            if (s[i] === s[j]) {
+                p[i] = j + 1;
+                break;
+            } else if (j === 0) {
+                p[i] = 0;
+                break;
+            }
+            j = p[j - 1];
+        }
     }
+
+    // search for div-occurrences
+    let start = div.length;
+    for (let i = start; i < s.length; i++) {
+        if (p[i] === div.length) {
+            result.push(s.slice(start, i - div.length + 1));
+            start = i + 1;
+        }
+    }
+    result.push(s.slice(start));
+
+    // return value
     return result;
 }
 
--- test/test.js
+++ test/test.js
@@ -16,4 +16,39 @@ describe("Split", function () {
             assert.deepEqual(str.split("."), split(str, "."));
         });
     });
+
+    describe("divider: multi-character", function () {
+        it('"aaababaaaaaababbabbabaaaaaababaaaaaaaabab", divider: "bab"', function () {
+            let str = "aaababaaaaaababbabbabaaaaaababaaaaaaaabab";
+            assert.deepEqual(str.split("bab"), split(str, "bab"));
+        });
+        it('"abracadabra", divider: "ab"', function () {
+            let str = "abracadabra";
+            assert.deepEqual(str.split("ab"), split(str, "ab"));
+        });
+        it('"abracadabra", divider: "abra"', function () {
+            let str = "";
+            assert.deepEqual(str.split("abra"), split(str, "abra"));
+        });
+        it('"", divider: "sostenuto"', function () {
+            let str = "";
+            assert.deepEqual(str.split("sostenuto"), split(str, "sostenuto"));
+        });
+        it('"", divider: ""', function () {
+            let str = "";
+            assert.deepEqual(str.split(""), split(str, ""));
+        });
+        it('"abra", divider: ""', function () {
+            let str = "abra";
+            assert.deepEqual(str.split(""), split(str, ""));
+        });
+        it('"", divider: none', function () {
+            let str = "";
+            assert.deepEqual(str.split(), split(str));
+        });
+        it('"abra", divider: none', function () {
+            let str = "";
+            assert.deepEqual(str.split(), split(str));
+        });
+    });
 });
